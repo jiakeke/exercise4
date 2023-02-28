f92e8c248318e44744d5fe8f7b5cbee419dfe7e6
Gary Jia <gary@Gary-MBP-New.local>
2023-03-01 01:39:08 +0800

    Add a an assignment1.py of exercise1 into repository
    Modify README.md a bit

---
+++ assignment1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Assignment 1
+# 
+# #### 7th Feb. 2023
+# ### **Ke Jia (Gary)**
+
+# ***
+# 
+# **The most commonly used programing languages:**
+# - Python
+# - Javascript
+# - C/C++
+# - Java
+# - Ruby
+# - Perl
+# - PHP
+
+# ***
+# | Intermediate devices | End devices | Media |
+# | :--- | :--- | :--- |
+# | Router | Laptop | Cables |
+# | Switch | iPhone | Wireless connections |
+# | NAT | iPad | Optical fiber |
+# | Firewalls | Apple Watch | Coaxial cable |
+# 
+
+# In[12]:
+
+
+print("Ke Jia")
+
+
+# In[13]:
+
+
+name = input("Please input your name: ")
+print("Hello", name)
+
+
+# In[11]:
+
+
+var_a = int(input("Please input number A: "))
+var_b = int(input("Please input number B: "))
+print("The sum of A + B is: ", var_a+var_b)
+
+
+# In[ ]:
+
+
+
+
